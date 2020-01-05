689c5196bc619ec7c052f36d3ef7afb5d5de118f
Tomasz Zubertowski <tzubertowski@gmail.com>
2020-01-05 15:02:24 +0000

    Implements FeatureList serialisation

--- src/app/Services/GeoFeatures/DTO/FeaturesList.php
+++ src/app/Services/GeoFeatures/DTO/FeaturesList.php
@@ -4,7 +4,31 @@
 namespace App\Services\GeoFeatures\DTO;
 
 
+use Illuminate\Support\Collection;
+
 class FeaturesList
 {
+    /** @var Collection[Feature] */
+    public $features;
+    /** @var int|null */
+    public $elapsedTime;
+    /** @var string|null */
+    public $type;
 
+    public function __construct()
+    {
+        $this->features = new Collection();
+    }
+
+    public function toArray()
+    {
+        return [
+            'data' => [
+                'features' => $this->features->toArray(),
+                'elapsed_time' => $this->elapsedTime,
+                'type' => $this->type,
+                'feature_count' => $this->features->count(),
+            ]
+        ];
+    }
 }
