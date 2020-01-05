65a2fb42b6fd15f77122b2eb5b2c47fad0e45314
Tomasz Zubertowski <tzubertowski@gmail.com>
2020-01-05 20:28:35 +0000

    Adds StringUtils to sanitize & help coerce the input + StringUtilsTest

---
+++ src/app/Services/GeoFeatures/StringUtils.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Services\GeoFeatures;
+
+
+class StringUtils
+{
+    /**
+     * Sanitizes the input string by unifying delimiters
+     *
+     * @param string $inputString
+     * @return string
+     */
+    public static function sanitizeInputString(string $inputString): string
+    {
+        $inputString = str_replace(' ', '', $inputString);
+        $inputString = preg_replace("![^a-z0-9_.]+!i", "-", $inputString);
+        return $inputString;
+    }
+
+    /**
+     * Gets coerced value from a string, eg. "0" returns 0 as an integer
+     *
+     * @param string $initialValue
+     * @return bool|float|int|string
+     */
+    public static function getCoercedValue(string $initialValue)
+    {
+        $initialValue = trim($initialValue);
+        if (empty($initialValue)) {
+            return 0;
+        }
+        if (!preg_match("/[^0-9.]+/", $initialValue)) {
+            if (preg_match("/[.]+/", $initialValue)) {
+                $double = (double)$initialValue;
+                return round($double, 6);
+            } else {
+                return (int)$initialValue;
+            }
+        }
+        if ($initialValue === "true" || $initialValue === "false") {
+            return (bool)$initialValue;
+        }
+        return (string)$initialValue;
+    }
+}
---
+++ src/tests/Services/GeoFeatures/StringUtilsTest.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Tests\Services\GeoFeatures;
+
+
+use App\Services\GeoFeatures\StringUtils;
+use Tests\TestCase;
+
+class StringUtilsTest extends TestCase
+{
+    /**
+     * @dataProvider provideCoerceData
+     * @param $input
+     * @param $expectedOutput
+     */
+    public function testGetsCoercedValue($input, $expectedOutput)
+    {
+        $this->assertEquals($expectedOutput, StringUtils::getCoercedValue($input));
+    }
+
+    public function provideCoerceData(): array
+    {
+        return [
+            ['1', 1],
+            ['a', 'a'],
+            ['', 0],
+            ['0.1', 0.1],
+            ['0.1231231', 0.123123],
+        ];
+    }
+
+    /**
+     * @dataProvider provideSanitizationData
+     * @param string $input
+     * @param string $expectedOutput
+     */
+    public function testSanitizesInputString(string $input, string $expectedOutput): void
+    {
+        $this->assertEquals($expectedOutput, StringUtils::sanitizeInputString($input));
+    }
+
+    public function provideSanitizationData(): array
+    {
+        return [
+            ['foobar', 'foobar'],
+            ['foo%', 'foo-'],
+            ['foo-', 'foo-'],
+            ['foo-1.23', 'foo-1.23'],
+            ['foo=1.23', 'foo-1.23'],
+            ['foo_bar=bar', 'foo_bar-bar'],
+            ['foo_bar=1.23,ke-1=21', 'foo_bar-1.23-ke-1-21'],
+        ];
+    }
+}
