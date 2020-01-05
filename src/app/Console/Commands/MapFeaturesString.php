6729256041f6945384f67aa144003e0efced95b4
Tomasz Zubertowski <tzubertowski@gmail.com>
2020-01-05 12:55:22 +0000

    Makes command callable

--- src/app/Console/Commands/MapFeaturesString.php
+++ src/app/Console/Commands/MapFeaturesString.php
@@ -6,12 +6,13 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class ParseFeatures extends Command
+class MapFeaturesString extends Command
 {
     protected $signature = 'features:map-to-json';
     protected $description = 'Parses strings of features and maps it into a JSON string';
 
-    public function handle() {
-        $this->info('Parsing input string');
+    public function handle()
+    {
+        $this->info('Parsing input features string');
     }
 }
