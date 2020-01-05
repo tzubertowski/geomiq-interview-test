5a45467164469687ac5fb639a0a972020b9d287a
Tomasz Zubertowski <tzubertowski@gmail.com>
2020-01-05 12:55:12 +0000

    Expects command to be callable

--- src/tests/Console/Commands/MapFeaturesStringTest.php
+++ src/tests/Console/Commands/MapFeaturesStringTest.php
@@ -1,7 +1,32 @@
 <?php
 
+namespace Tests\Console\Commands;
 
-class MapFeaturesStringTest
+use App\Console\Commands\MapFeaturesString;
+use Tests\ConsoleTestCase;
+
+class MapFeaturesStringTest extends ConsoleTestCase
 {
+    /**
+     * System under test
+     * @var MapFeaturesString
+     */
+    private $sut;
 
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->sut = app(MapFeaturesString::class);
+    }
+
+    public function testCommandIsCallable(): void
+    {
+        $this->assertEquals(0, $this->artisan('features:map-to-json'));
+    }
+
+    public function testCommandReturnsOutput(): void
+    {
+        $sutOutput = $this->runCommand($this->sut);
+        $this->assertStringContainsString('Parsing input features string', $sutOutput->getDisplay());
+    }
 }
--- src/tests/ConsoleTestCase.php
+++ src/tests/ConsoleTestCase.php
@@ -1,7 +1,19 @@
 <?php
 
+namespace Tests;
 
-class ConsoleTestCase
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Tester\CommandTester;
+
+abstract class ConsoleTestCase extends TestCase
 {
+    protected function runCommand(Command $command, array $arguments = [], array $interactiveInput = []): CommandTester
+    {
+        $command->setLaravel($this->app);
+        $tester = new CommandTester($command);
+        $tester->setInputs($interactiveInput);
+        $tester->execute($arguments);
 
+        return $tester;
+    }
 }
